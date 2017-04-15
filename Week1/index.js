8dad39ca54b8d682e897c0ee3bdad648726f8d37
Jennifer Paulino <jenpaulino3@gmail.com>
2017-04-15 17:21:26 -0400

    First commit

---
+++ Week1/index.js
@@ -0,0 +1,71 @@
+// Stores the numbers
+var box = document.getElementById("display");
+	// To display clicked #
+
+function addtoscreen(x){
+	box.value += x;
+
+	if(x=='C'){
+		box.value = '';
+	}
+}
+
+function answer(){
+	x = box.value;
+	x = eval(x);
+	box.value = x;
+
+}
+
+function backspace(){
+	var number = box.value;
+	var len = number.length-1;
+	var newnumber = number.substring(0,len);
+	// Substring extracts the characters and returns new string. Requires start and end
+	box.value=newnumber;
+}
+
+
+
+/*var num1;
+var num2;
+var res;
+var results;
+
+document.addEventListener("DOMContentLoaded", function(){
+	num1 = document.getElementById("num1");
+	num2 = document.getElementById("num2");
+	res = document.getElementById("results");
+});
+
+function add(){
+	// console.log("Let's add");
+	// var math = 7 + 7;
+	// console.log(math);
+	results = +num1.value + +num2.value;
+	res.value = results;
+}
+
+function subtract(){
+	// console.log("Let's subtract");
+	// var math = 7 - 7;
+	// console.log(math);
+	results = +num1.value - +num2.value;
+	res.value = results;
+}
+
+function multiply(){
+	// console.log("Let's multiply");
+	// var math = 7 * 7;
+	// console.log(math);
+	results = +num1.value * +num2.value;
+	res.value = results;
+}
+
+function divide(){
+	// console.log("Let's divide");
+	// var math = 7 / 7;
+	// console.log(math);
+	results = +num1.value / +num2.value;
+	res.value = results;
+}*/
